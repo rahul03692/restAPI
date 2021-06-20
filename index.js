846c79b0e1932c7f5355a04d8fe16059d6e4a5f8
rahul03692 <rahul03692@gmail.com>
2021-06-20 22:20:48 +0530

    heroku

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
-const express=require("express");
-const bodyParser=require("body-parser");
-const cors=require("cors");
-const dotenv=require("dotenv");
-const mongoose=require("mongoose");
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-dotenv.config({path:"./config.env"});
+dotenv.config({ path: "./config.env" });
 
-mongoose.connect(process.env.DATABASE, {useNewUrlParser: true}).then(()=>console.log("DB successful"));
+mongoose
+  .connect(process.env.DATABASE, { useNewUrlParser: true })
+  .then(() => console.log("DB successful"));
 
+const routes = require("./routes/api");
 
-const routes=require("./routes/api");
-const { response } = require("express");
+const app = express();
 
-const app=express();
+app.enable("trust proxy");
 
+app.use(express.json());
+app.use("/api", routes);
 
-app.use(cors());
-
-app.use(bodyParser.json());
-app.use("/api",routes);
-
-app.use(function(err,req,res,next){
-    res.status(422).send({error:err.message});
+app.get("/", () => {
+  res.send("go to /api to see data");
 });
 
+app.use(function (err, req, res, next) {
+  res.status(422).send({ error: err.message });
+});
 
-app.get('/',()=>{
-    res.send("go to /api to see data");
-})
-
-app.listen(process.env.PORT || 5000,function(){
-    console.log("server is running on 5000");
+app.listen(process.env.PORT || 5000, function () {
+  console.log("server is running on 5000");
 });
